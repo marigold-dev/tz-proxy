fa02b55425db03d7d3e2c60bd02b25d33499db64
Felipe Cardozo <fraifelipe@gmail.com>
2022-10-21 10:45:21 -0300

    fix nix stuff

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -13,33 +13,33 @@ in {
           prev.lib.lists.remove osuper.uuidm o.propagatedBuildInputs;
       });
 
-      uri = osuper.uri.overrideAttrs (o: {
-        propagatedBuildInputs = (with osuper; [ ounit ])
-          ++ o.propagatedBuildInputs;
-      });
+      # uri = osuper.uri.overrideAttrs (o: {
+      #   propagatedBuildInputs = (with osuper; [ ounit ])
+      #     ++ o.propagatedBuildInputs;
+      # });
 
-      piaf = osuper.piaf.overrideAttrs (o: {
-        src = prev.fetchFromGitHub {
-          owner = "anmonteiro";
-          repo = "piaf";
-          rev = "be16b6752000f24e4bf393ea5e312551ff86b9fd";
-          sha256 = "sha256-HyhdblCb2p6H1nmme6u6aBx/Rqmyde/9mwjslp9gFX0=";
-          fetchSubmodules = true;
-        };
-        patches = [ ];
-        doCheck = false;
-        # propagatedBuildInputs = with osuper; [
-        #   eio
-        #   eio_main
-        #   eio-ssl
-        #   httpaf-eio
-        #   h2-eio
-        #   ipaddr
-        #   magic-mime
-        #   multipart_form
-        #   sendfile
-        #   uri
-        # ];
-      });
+      # piaf = osuper.piaf.overrideAttrs (o: {
+      #   src = prev.fetchFromGitHub {
+      #     owner = "anmonteiro";
+      #     repo = "piaf";
+      #     rev = "be16b6752000f24e4bf393ea5e312551ff86b9fd";
+      #     sha256 = "sha256-HyhdblCb2p6H1nmme6u6aBx/Rqmyde/9mwjslp9gFX0=";
+      #     fetchSubmodules = true;
+      #   };
+      #   patches = [ ];
+      #   doCheck = false;
+      #   # propagatedBuildInputs = with osuper; [
+      #   #   eio
+      #   #   eio_main
+      #   #   eio-ssl
+      #   #   httpaf-eio
+      #   #   h2-eio
+      #   #   ipaddr
+      #   #   magic-mime
+      #   #   multipart_form
+      #   #   sendfile
+      #   #   uri
+      #   # ];
+      # });
     })) prev.ocaml-ng;
 }
