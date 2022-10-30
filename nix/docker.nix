84b87be53fc0cacfb8f508b8bbd149f6e944c5b0
Felipe Cardozo <fraifelipe@gmail.com>
2022-10-30 16:49:31 -0300

    add os and architecture label in docker nix

--- nix/docker.nix
+++ nix/docker.nix
@@ -1,12 +1,12 @@
-
 { pkgs, tzproxy }:
 
 let
   baseImage = pkgs.dockerTools.pullImage {
     imageName = "debian";
-    imageDigest = "sha256:e25b64a9cf82c72080074d6b1bba7329cdd752d51574971fd37731ed164f3345";
+    imageDigest =
+      "sha256:e25b64a9cf82c72080074d6b1bba7329cdd752d51574971fd37731ed164f3345";
     sha256 = "sha256-Ql7LWpMyRxL3biq3WujAyRJhr80Zy1lFKV2yP8LK/q4=";
-    };
+  };
 
 in pkgs.dockerTools.buildImage {
   name = "ghcr.io/marigold-dev/tz-proxy";
@@ -18,6 +18,8 @@ in pkgs.dockerTools.buildImage {
 
   config = {
     author = "marigold.dev";
+    architecture = "amd64";
+    os = "linux";
     WorkingDir = "/app";
     Entrypoint = [ "${tzproxy}/bin/tzproxy" ];
   };
