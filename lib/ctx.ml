fd5302dad9df270c8a8a20b24583ecf166ab3d44
Felipe Cardozo <fraifelipe@gmail.com>
2022-11-08 21:26:21 -0300

    use one client for all proxy

--- lib/ctx.ml
+++ lib/ctx.ml
@@ -2,6 +2,7 @@ type t =
   { env : Eio.Stdenv.t
   ; storage : Memory_storage.t
   ; variables : Variables.t
+  ; client : Piaf.Client.t
   }
 
-let create env storage variables = { env; storage; variables }
+let create env storage variables client = { env; storage; variables; client }
